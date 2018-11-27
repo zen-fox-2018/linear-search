3f4624b31fc720e3057d79065e6d4e1a7e7a1b4d
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-27 11:25:46 +0700

    komit

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,13 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  
+  for (let i = 0; i < values.length; i++) {
+    if (target == values[i]) {
+      return i
+    }  
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +22,13 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  let arr = []
+  for (let i = 0; i < values.length; i++) {
+    if (target.toLowerCase() === values[i].toLowerCase()) {
+      arr.push(i)
+    }
+  }
+  return arr
 }
 
 let banana_arr = "banana".split("");
