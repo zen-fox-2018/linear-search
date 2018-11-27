afbda7adde504b4dfa18ee961c328e6067d1a8f4
rahmanhasri <rahman.hasri@gmail.com>
2018-11-27 14:51:38 +0700

    maman-linear

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,27 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  // values = values.sort(function(a,b){return a>b})
+
+  // insertion sort here
+  for (let i = 0 ; i < values.length -1 ; i++){
+    for (let j = i + 1 ; j >= 0 ; j--){
+      if (values[j] < values[j-1]){
+        let tmp = values[j]
+        values[j] = values[j-1]
+        values[j-1] = tmp
+      }
+    }
+  }
+  // console.log(values)
+
+  let output = -1
+  for (let i = 0 ; i <values.length ; i++){
+    if (values[i] == target){
+      return values[0]
+    }
+  }
+  return output
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +38,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let output = []
+  for (let i = 0 ; i < values.length ; i++){
+    if (values[i] === target){
+      output.push(i)
+    }
+  }
+  return output
 }
 
 let banana_arr = "banana".split("");
