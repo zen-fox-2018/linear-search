4f6033d4220cb457f51a48618e0c536b577aa198
Arief Rachman <arievrchman@gmail.com>
2018-11-27 20:26:44 +0700

    done

--- linear_search.js
+++ linear_search.js
@@ -4,9 +4,15 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      return i
+    }
+  }
+  return -1
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -17,6 +23,14 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let indexOfTarget = [];
+
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      indexOfTarget.push(i)
+    }
+  }
+  return indexOfTarget
 }
 
 let banana_arr = "banana".split("");
