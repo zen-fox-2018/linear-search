0140ea0565b851aa57f6b6c1f0e86617f5d6b702
Celyn Susanto <celyn.vy@gmail.com>
2018-11-27 16:53:14 +0700

    linear search done

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,14 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  var result = -1
+  for (var i = 0; i <= values.length-1; i++){
+      if (values[i] == target) {
+          // console.log(values[i])
+          return i
+      }
+  }
+  return result
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +25,14 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var result = []
+
+  for (var i = 0; i <= values.length-1; i++) {
+      if (values[i] == target) {
+          result.push(i)
+      }
+  }
+  return result
 }
 
 let banana_arr = "banana".split("");
