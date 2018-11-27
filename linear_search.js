76e79def4ac5ce9586041fdf1ca2f2df4a6a307b
Mahdihrs <mahdiharis@outlook.com>
2018-11-27 21:06:19 +0700

    linear search

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,18 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  var result = 0
+  for (let i = 0; i < random_numbers.length; i++) {
+    var found = true
+    if (target === random_numbers[i]) {
+      debugger
+      return i
+    } 
+    else {
+      found = false
+    }
+  }
+  return (found === false) ? -1 : result[0]
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +27,13 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var result = []
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      result.push(i)
+    }
+  }
+  return result
 }
 
 let banana_arr = "banana".split("");
